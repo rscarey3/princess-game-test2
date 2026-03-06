aa0c974dc383d5ff1bc77d6d8ce18035205e14b7
rscarey3 <rscarey3@gmail.com>
2026-03-06 12:29:41 +0000

    problemWithBulletsFiring
    Bullets are neither being destroyed nor are they actually leaving the firing point and flying towards the enemy.

--- Assets/Scripts/BulletTest.cs
+++ Assets/Scripts/BulletTest.cs
@@ -10,11 +10,24 @@ public class BulletTest : MonoBehaviour
 
     private Transform target;
 
+    public void SetTargetTest(Transform _target)
+    {
+        target = _target;
+    }
+
     private void FixedUpdate()
     {
+        if (!target) return;
+
         Vector2 direction = (target.position - transform. position).normalized;
 
         rb.linearVelocity = direction * bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //Take Health From Enemy
+        Destroy(gameObject);
     }
     
 }
